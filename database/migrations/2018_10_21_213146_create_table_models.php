4621605fa2656374708f99d264f19cc82ee87ff9
César Hernández Rocha <cesarhrocha96@gmail.com>
2018-10-21 16:48:51 -0500

    REST models lista

---
+++ database/migrations/2018_10_21_213146_create_table_models.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableModels extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('model_name')->unique();
+            $table->integer('brand_id')->references('id')->on('brands');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
